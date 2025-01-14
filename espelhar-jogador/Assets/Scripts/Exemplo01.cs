c24a26cf9eb3aaff461937da8ace6612c0adcb35
GabrielChiarelli <102618272+GabrielChiarelli@users.noreply.github.com>
2025-01-14 13:32:42 -0300

    Projeto inicial

---
+++ espelhar-jogador/Assets/Scripts/Exemplo01.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exemplo01 : MonoBehaviour
+{
+    [Header("Referências Gerais")]
+    private Rigidbody2D oRigidbody2D;
+
+    [Header("Movimentação")]
+    [SerializeField] private float velocidadeDeMovimento = 8.0f;
+    private float inputHorizontal = 0.0f;
+
+    private void Start()
+    {
+        oRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        ReceberInputs();
+    }
+
+    private void ReceberInputs()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        MovimentarJogador();
+    }
+
+    private void MovimentarJogador()
+    {
+        oRigidbody2D.velocity = new Vector2(inputHorizontal * velocidadeDeMovimento, oRigidbody2D.velocity.y);
+    }
+}
